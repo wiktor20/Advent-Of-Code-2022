2015c4f20c62de3037ad97ed98f4eab088d2dd85
wiktor20 <victor.kolcan@gmail.com>
2022-12-07 14:11:56 -0600

    First AOC1 Part 1 Done

--- AOC1/main.cpp
+++ AOC1/main.cpp
@@ -1,10 +1,35 @@
 #include <fstream>
 #include <iostream>
 
-
-
-int main(int argc, char const *argv[])
+int main()
 {
+    int max = 0;
+    int tempsum = 0;
+    std::ifstream inf{"input.txt"};
 
+    // If we couldn't open the output file stream for reading
+    if (!inf)
+    {
+        std::cerr << "txt could not be opened for reading\n";
+        return 1;
+    }
+
+    while (inf)
+    {
+        std::string strInput;
+        std::getline(inf, strInput);
+
+        if (!strInput.empty())
+            tempsum += std::stoi(strInput);
+
+        else
+        {
+            if (tempsum > max)
+                max = tempsum;
+            tempsum = 0;
+        }
+    }
+
+    std::cout << max;
     return 0;
 }
