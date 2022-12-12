d1d5db923b5e452625206c8b833ed59b43380e10
wiktor20 <victor.kolcan@gmail.com>
2022-12-11 23:04:27 -0600

    AOC 6 Part 2 Done

--- AOC6/main.cpp
+++ AOC6/main.cpp
@@ -1,12 +1,29 @@
 #include <fstream>
 #include <iostream>
-#include <map>
+
+int function(int currentchar, std::string test, int size)
+{
+    if (currentchar != 0)
+    {
+        if (function(currentchar - 1, test, size) == false)
+        {
+            return false;
+        }
+        for (int i = 0; i < currentchar; i++)
+        {
+            if (test[size - currentchar] == test[size - currentchar + i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    return true;
+}
+
 
 int main()
 {
-    bool a = false;
-    bool b = false;
-    bool c = false;
     std::ifstream inf{"input.txt"};
     // If we couldn't open the output file stream for reading
     if (!inf)
@@ -14,56 +31,20 @@ int main()
         std::cerr << "txt could not be opened for reading\n";
         return 1;
     }
-    std::string test;
-    std::string strInput;
-    getline(inf, strInput);
     int i = 0;
-    int temp = 0;
+    int size = 14;
+    std::string strInput, test;
+    getline(inf, strInput);
+
     while (1)
     {
-        temp = 0;
-        test.assign(strInput, 0 + i, 4 + i);
-        std::cout << "First:" << std::endl;
-        for (int i = 0; i < 3; i++)
-        {
-            std::cout << test[0] << test[i + 1];
-            if (test[0] != test[i + 1]){
-                std::cout << "+1";
-                temp++;
-            }
-            if (temp == 3)
-                a = true;
-            std::cout << "temp: " << temp; 
-            std::cout <<  std::endl;
-            
-        }
-        temp = 0;
-        std::cout << "Second:" << std::endl;
-        for (int i = 0; i < 2; i++)
-        {
-
-            std::cout << test[1] << test[i + 2];
-            if (test[1] != test[i + 2]){
-                std::cout << "+1";
-                temp++;
-            }
-            if (temp == 2)
-                b = true;
-            std::cout <<  std::endl;
-        }
-        if (test[2] != test[3])
-        {
-            c = true;
-        }
-        // std::cout << test;
-        std::cout << a << b << c << std::endl;
-        if (a && b && c)
+        test.assign(strInput, i + 0, size);
+        if (test.size() < size)
+            break;
+        if (function(size - 1, test, size - 1) == true)
             break;
         i++;
-        a = false;
-        b = false;
-        c = false;
     }
-    std::cout << i + 4;
+    std::cout << i + size;
     return 0;
 }
